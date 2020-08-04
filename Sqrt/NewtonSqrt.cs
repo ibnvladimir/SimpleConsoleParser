ab3078a696af77aad5abd225c760c357b6be2492
ibnvladimir <iidesign@yandex.ru>
2020-08-04 16:50:36 +0400

    done

--- Sqrt/NewtonSqrt.cs
+++ Sqrt/NewtonSqrt.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Sqrt
 {
-    class NewtonSqrt
+    public class NewtonSqrt
     {
-        double Pow(double x0, int exponent)
+        public double Pow(double x0, int exponent)
         {
             double result = 1;
             for (int i = 0; i < exponent; i++) 
@@ -18,24 +18,23 @@ namespace Sqrt
             return result;
         }
 
-        public  double NewtonSqrtMethod(int exponent, double baseNumber, double accuracy = 0.00001)
+        public  double NewtonSqrtMethod(int exponent, double baseNumber, double accuracy = 0.01)
         {
-            var x0 = baseNumber/exponent;
+            var x0 = Math.Sqrt(baseNumber);
             var x1 = NewtonSqrtFormula(exponent, baseNumber, x0);
-
-            while ((x1 - x0) > accuracy)
+            while (Math.Abs(x1 - x0) > accuracy)
             {
                 x0 = x1;
                 x1 = NewtonSqrtFormula(exponent, baseNumber, x0);
             }
-
             return x1;
         }
 
-        private  double NewtonSqrtFormula(int exponent, double baseNumber, double x0)
+        public double NewtonSqrtFormula(int exponent, double baseNumber, double x0)
         {
             double pow = Pow(x0, exponent - 1);
-            double result = (1 / exponent) * ((exponent - 1) * x0 + baseNumber / pow);
+            double result = (1f / exponent) * ((exponent - 1) * x0 + baseNumber / pow);
+                                                                  
             return result; 
         }
     }
--- Sqrt/Program.cs
+++ Sqrt/Program.cs
@@ -6,78 +6,145 @@ using System.Threading.Tasks;
 
 namespace Sqrt
 {
-    class Program
+    public class Program
     {
         
         static void Main(string[] args)
         {
             double baseNumber;
             int exponent;
+            double accuracy;
 
             while (true)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Введите число из которого хотите извлечь корень или @ для выхода из программы:");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("> ");
-                var baseNumberAsSring = Console.ReadLine();
-                if (baseNumberAsSring == "@") { return; }
-                if (String.IsNullOrWhiteSpace(baseNumberAsSring)) { continue; }
+                while (true)//InputBaseNumber
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Введите число из которого хотите извлечь корень или @ для выхода из программы:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("> ");
+                    var baseNumberAsSring = Console.ReadLine();
+                    if (baseNumberAsSring == "@") { return; }
+                    if (String.IsNullOrWhiteSpace(baseNumberAsSring)) { continue; }
 
-                try
-                {
-                    baseNumber = double.Parse(baseNumberAsSring);
-                }
-                catch (FormatException)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Что-то не так с этим числом!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    continue;
-                }
-                catch (OverflowException)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Слишком много знаков!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    continue;
+                    try
+                    {
+                        baseNumber = double.Parse(baseNumberAsSring);
+                        if (baseNumber <= 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Я не умею извлекать корень из отрицательного числа и 0!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            continue;
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Что-то не так с этим числом!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Слишком много знаков!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
                 }
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Введите степень корня (целое число) или @ для выхода из программы:");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("> ");
-                var exponentAsString = Console.ReadLine();
-                if (exponentAsString == "@") { return; }
-                if (String.IsNullOrWhiteSpace(exponentAsString)) { continue; }
+                while (true) //InputExponent
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Введите степень корня (целое число) или @ для выхода из программы:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("> ");
+                    var exponentAsString = Console.ReadLine();
+                    if (exponentAsString == "@") { return; }
+                    if (String.IsNullOrWhiteSpace(exponentAsString)) { continue; }
 
-                try
-                {
-                    exponent = int.Parse(exponentAsString);
+                    try
+                    {
+                        exponent = int.Parse(exponentAsString);
+                        if (exponent <= 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Я не умею извлекать корни отрицательной степени и со степенью 0!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            continue;
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Что-то не так с этим числом!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Слишком много знаков!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+
+                    
+                   
+
                 }
-                catch (FormatException)
+
+                while (true) //InputAccuracy
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Что-то не так с этим числом!");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Введите желаемую точность расчета (по умолчанию 0.01) или @ для выхода из программы:");
                     Console.ForegroundColor = ConsoleColor.White;
-                    continue;
+                    Console.Write("> ");
+                    var accuracyAsString = Console.ReadLine();
+                    if (accuracyAsString == "@") { return; }
+                    if (String.IsNullOrWhiteSpace(accuracyAsString))
+                    {
+                        Console.WriteLine("> принято значение по умолчанию = 0.01");
+                        accuracy = 0.01;
+                        break;
+                    }
+
+                    try
+                    {
+                        accuracy = double.Parse(accuracyAsString);
+                        if (accuracy <= 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Отрицательная и нулевая точность не допустимы!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            continue;
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Что-то не так с этим числом!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Слишком много знаков!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    }
                 }
-                catch (OverflowException)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Слишком много знаков!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    continue;
-                }
+
                 var newton = new NewtonSqrt();
-                Console.WriteLine("Вычисленно по алгоритму Ньютона:" + newton.NewtonSqrtMethod(exponent, baseNumber));
+                Console.WriteLine("Вычисленно по алгоритму Ньютона:" + newton.NewtonSqrtMethod(exponent, baseNumber, accuracy));
                 Console.WriteLine("Стандартный метод C#           :" + Math.Pow(baseNumber, 1f / exponent));
-
             }
-           
-            
-            
-        
+
         }
     }
 }
