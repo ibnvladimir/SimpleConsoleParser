b6db8c54b1aab083f7e17ec421ac5487351db53e
ibnvladimir <iidesign@yandex.ru>
2020-08-10 14:03:49 +0400

    Done

---
+++ parserTtests/GreatestCommonFactor_Test.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GreatestCommonFactor;
+
+namespace parserTtests
+{
+    [TestClass]
+    public class GreatestCommonFactor_Test
+    {
+        [TestMethod]
+        public void ShouldReturnGreatestCommonFactor_Of2Integers()
+        {
+            // Arrange
+            var gcf = new GreatestCommonFactorCounter();
+
+            // Act
+            var actualResult = gcf.FactorCounter(6, 12);
+            var expectedResult = 6;
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ShouldReturnGreatestCommonFactor_OfThreeOrMoreIntegers()
+        {
+            // Arrange
+            var gcf = new GreatestCommonFactorCounter();
+
+            // Act
+            var actualResult = gcf.FactorCounter(5, 15, new int[] {30, 100500} );
+            var expectedResult = 5;
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ShouldReturnGreatestCommonFactor_SteinAlgorithm()
+        {
+            // Arrange
+            var gcf = new GreatestCommonFactorCounter();
+
+            // Act
+            var actualResult = gcf.Stein(5, 15);
+            var expectedResult = 5u;
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+}
